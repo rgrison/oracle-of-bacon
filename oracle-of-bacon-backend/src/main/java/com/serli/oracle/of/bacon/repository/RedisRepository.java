8c071b0e30db895490f5799ab66c2504bac399d3
austil <augustin@mrlacour.fr>
2019-02-19 15:44:04 +0100

    Redis: last 10 search

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/RedisRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/RedisRepository.java
@@ -2,17 +2,24 @@ package com.serli.oracle.of.bacon.repository;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class RedisRepository {
     private final Jedis jedis;
+    private final static String KEY = "SearchHistory";
 
     public RedisRepository() {
         this.jedis = new Jedis("localhost");
     }
 
+    public void saveSearch(String query) {
+        jedis.lpush(KEY, query);
+        jedis.ltrim(KEY, 0, 9);
+    }
+
     public List<String> getLastTenSearches() {
-        // TODO implement last 10 searchs
-        return null;
+        String[] last10 = jedis.lrange(KEY, 0, -1).toArray(new String[0]);
+        return Arrays.asList(last10);
     }
 }
